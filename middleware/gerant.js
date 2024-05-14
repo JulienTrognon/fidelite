55a04065a4cf85e643e13c5411986d90c4862c95
JULIEN TROGNON <jlntrog@gmail.com>
2024-05-14 11:24:30 +0200

    fonction suppprimer client

--- middleware/gerant.js
+++ middleware/gerant.js
@@ -4,14 +4,24 @@ const { query } = require('../db/connect');
 const gerant = {
   async get_liste_clients(req, res) {
     const { rows } = await query(
-      'SELECT nom, prenom, pseudo, email FROM client ORDER BY nom',
+      'SELECT id, nom, prenom, pseudo, email FROM client ORDER BY nom',
     );
     return rows;
   },
 
   async ajouter_client(req, res) {
     
-  }
+  },
+
+  async supprimer_client(req, res) {
+    const { id } = req.body; // récupération des données
+    try {
+      await db.query('DELETE FROM client WHERE id = $1', [id]); // requête SQL
+      }
+    catch (err) { // si erreur
+      console.error(err); // affichage de l'erreur
+    }
+  },
 }
 
 module.exports = gerant;
